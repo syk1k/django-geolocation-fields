4d9174c9df72381023751052137743b8804b7725
syk1k <dambemondo@gmail.com>
2020-01-22 15:32:11 -0900

    Add field options

--- geolocation_fields/forms/widgets.py
+++ geolocation_fields/forms/widgets.py
@@ -1,5 +1,12 @@
 from django.forms import widgets
+from geolocation_fields.settings import *
 
 class PointWidget(widgets.Input):
     input_type = 'text'
     template_name = 'widgets/point.html'
+
+    def get_context(self, name, value, attrs):
+        print(GEOLOCATION_FIELD)
+        context = super().get_context(name, value, attrs)
+        context['provider'] = GEOLOCATION_FIELD['provider']
+        return context
--- geolocation_fields/settings.py
+++ geolocation_fields/settings.py
@@ -2,11 +2,21 @@ from django.conf import settings
 
 
 # Check to see if the GEOLOCATION_FIELD is provided in the settings
+avalaible_options = ['provider', 'center', 'zoom']
 try:
     GEOLOCATION_FIELD = settings.GEOLOCATION_FIELD
+    for option in avalaible_options:
+        value = GEOLOCATION_FIELD.get(option, None)
+        if value is None:
+            if option=='provider':
+                GEOLOCATION_FIELD[option] = 'leaflet'
+            if option=='center':
+                GEOLOCATION_FIELD[option] = [0,0]
+            if option=='zoom':
+                GEOLOCATION_FIELD[option] = 12
 except:
     GEOLOCATION_FIELD = {
-        "map_provider": "leaflet", # Two provider: Leaflet, Mapbox
+        "provider": "leaflet", # Two provider: Leaflet, Mapbox
         "center" : [0,0],
         "zoom": 12,
     }
