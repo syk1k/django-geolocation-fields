c688aef9b96f8f537f9a3094fa1350d3b583aed1
syk1k <dambemondo@gmail.com>
2020-01-27 13:54:56 -0900

    Add a comment

--- geolocation_fields/static/widgets/leaflet_implementations/point.js
+++ geolocation_fields/static/widgets/leaflet_implementations/point.js
@@ -5,7 +5,9 @@ $(document).ready(()=>{
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+    
 
+    // Get the value of the point to edit or set a new point to coordinate [0,0]
     var marker_value;
     if(point_value){
         marker_value = point_value;        
