b23930b9083c0d7894048fcfe2d706a10688fdcd
syk1k <dambemondo@gmail.com>
2020-01-29 06:56:57 -0900

    Documentation with Sphinx

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-geolocation-fields", # Replace with your own username
-    version="0.1.4",
+    version="0.1.5",
     author="GOUNTENI DAMBE TCHIMBIANDJA",
     author_email="dambemondo@gmail.com",
     description="A Django app to work with geolocation fields without a spatial database.",
