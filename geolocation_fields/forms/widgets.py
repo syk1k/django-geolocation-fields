5479838b080c13a60d17dfda79172ba21ee0ba8e
syk1k <dambemondo@gmail.com>
2020-01-30 04:48:48 -0900

    Change configuratons variable name

--- geolocation_fields/forms/widgets.py
+++ geolocation_fields/forms/widgets.py
@@ -7,8 +7,9 @@ class PointWidget(widgets.Input):
 
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
-        context['provider'] = GEOLOCATION_FIELD['provider']
-        context['display_input'] = GEOLOCATION_FIELD['display_input']
+        context['provider'] = GEOLOCATION_FIELDS['provider']
+        context['display_input'] = GEOLOCATION_FIELDS['display_input']
+        context['center'] = GEOLOCATION_FIELDS['center']
         if value:
             context['point_value'] = value
         return context
--- geolocation_fields/settings.py
+++ geolocation_fields/settings.py
@@ -1,11 +1,11 @@
 from django.conf import settings
 
 
-# Check to see if the GEOLOCATION_FIELD is provided in the settings
+# Check to see if the GEOLOCATION_FIELDS is provided in the settings
 avalaible_options = ['provider', 'center', 'zoom', 'display_input']
 
 # Default configurations
-GEOLOCATION_FIELD = {
+GEOLOCATION_FIELDS = {
     "provider": "leaflet", # Two provider: Leaflet, Mapbox
     "center" : [0,0],
     "zoom": 12,
@@ -13,8 +13,8 @@ GEOLOCATION_FIELD = {
 }
 try:
     for option in avalaible_options:
-        value = settings.GEOLOCATION_FIELD.get(option, None)
+        value = settings.GEOLOCATION_FIELDS.get(option, None)
         if value:
-            GEOLOCATION_FIELD[option] = value                
+            GEOLOCATION_FIELDS[option] = value                
 except:
     pass
