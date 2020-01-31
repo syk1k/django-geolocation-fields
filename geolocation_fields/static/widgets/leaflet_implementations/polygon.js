6bdd612724bea49ba626a06a1700f289c27db17c
syk1k <dambemondo@gmail.com>
2020-01-31 05:54:30 -0900

    Remove downloaded static files to make the package lighter

--- geolocation_fields/static/widgets/leaflet_implementations/polygon.js
+++ geolocation_fields/static/widgets/leaflet_implementations/polygon.js
@@ -1,5 +1,6 @@
 $(document).ready(()=>{
     var attribution = polygon_map_attributions;
+    var polygon_field = $(`#id_${polygon_input_widget}`)
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -13,15 +14,34 @@ $(document).ready(()=>{
         [6.17292, 1.21390]
     ]).addTo(polygon_map);
 
-    console.log(polygon.getLatLngs())
     
 
     polygon.on('click', ()=>{
-        console.log(polygon.getLatLngs())
-    })
+        polygonToArray = []
+        polygonLatLng = polygon.getLatLngs()[0];
+        
+        polygonLatLng.forEach(corner => {
+            console.log(corner)
+            polygonToArray.push(`[ ${corner.lat}, ${corner.lng}]`)
+        });
 
-    polygon_map.on('load',()=>{
-        console.log("Loaded")
-    })    
+        polygon_field.val(`[${polygonToArray}]`)
+    })
     
+    
+    // Icontrol for Polygon creation
+
+    class MapManageControl extends L.Control{
+        htmlElement = document.createElement('div');
+        
+        onAdd = (map) =>{
+            this.htmlElement.className = 'map_manage_control leaflet-control-container';
+            this.htmlElement.appendChild(document.querySelector("#map_manage_control"));
+            return this.htmlElement
+        }
+    }
+
+
+    let map_manage_control = new MapManageControl();
+    map_manage_control.addTo(polygon_map);    
 });
