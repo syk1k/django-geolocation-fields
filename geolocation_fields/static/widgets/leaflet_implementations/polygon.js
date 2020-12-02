714ae906e73f909e22968d54a06554529a148f98
syk1k <dambemondo@gmail.com>
2020-12-02 17:33:20 +0000

    Add Editable for Polygon Drawing: OK

--- geolocation_fields/static/widgets/leaflet_implementations/polygon.js
+++ geolocation_fields/static/widgets/leaflet_implementations/polygon.js
@@ -1,32 +1,8 @@
 $(document).ready(() => {
-    var attribution = polygon_map_attributions;
-    var polygon_field = $(`#id_${polygon_input_widget}`)
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(polygon_map);
-
-
-    // Polygone test
-    /* var polygon = L.polygon([
-        [6.17357, 1.21276],
-        [6.17375, 1.21395],
-        [6.17292, 1.21390]
-    ]).addTo(polygon_map); */
-
-
-
-    polygon.on('click', () => {
-        polygonToArray = []
-        polygonLatLng = polygon.getLatLngs()[0];
-
-        polygonLatLng.forEach(corner => {
-            console.log(corner)
-            polygonToArray.push(`[ ${corner.lat}, ${corner.lng}]`)
-        });
-
-        polygon_field.val(`[${polygonToArray}]`)
-    })
 
     L.EditControl = L.Control.extend({
 
@@ -54,16 +30,17 @@ $(document).ready(() => {
 
     });
 
-    var lineControl = L.EditControl.extend({
+
+    L.NewPolygonControl = L.EditControl.extend({
+
         options: {
             position: 'topleft',
-            callback: map.editTools.startPolyline,
-            kind: 'line',
-            html: '\\/\\'
+            callback: polygon_map.editTools.startPolygon,
+            kind: 'polygon',
+            html: '▰'
         }
 
     });
 
-    map.addControl(lineControl);
-
+    polygon_map.addControl(new L.NewPolygonControl());
 });
